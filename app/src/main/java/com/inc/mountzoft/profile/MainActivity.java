c7b6bafa9086a23214c2f6473b2ba8a666a52afd
Jithin K Jose <jithin.jude68@gmail.com>
2018-03-16 23:49:35 +0530

    Initial commit

---
+++ app/src/main/java/com/inc/mountzoft/profile/MainActivity.java
@@ -0,0 +1,18 @@
+package com.inc.mountzoft.profile;
+
+import android.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.action_bar_layout);
+    }
+}
